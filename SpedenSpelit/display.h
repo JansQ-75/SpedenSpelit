f503ff2076f812849d0420611fe7cdbd52953fc5
JansQ-75 <janina7590@hotmail.com>
2024-10-02 18:37:29 +0300

    fixed typo in display.h

--- SpedenSpelit/display.h
+++ SpedenSpelit/display.h
@@ -61,8 +61,8 @@ void writeHighAndLowNumber(uint8_t tens,uint8_t ones);
 void showResult(byte result);
 
 /*
-  gameOver() subroutine is called when player loses the game.
-  Subroutine writes text "Loser...see ya" to 7-segment display. 
+  gameOver() function is called when player loses the game.
+  Function writes text "Loser...see ya" to 7-segment display. 
   Text is shown letter by letter.
 */
 void gameOver();
