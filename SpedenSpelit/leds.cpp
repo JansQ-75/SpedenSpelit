5c2a047a3d9e32bc0bc4d4c06e38d75b728e3da2
OsMO9 <156901442+OsMO9@users.noreply.github.com>
2024-10-09 18:17:34 +0300

    Update leds.cpp

--- SpedenSpelit/leds.cpp
+++ SpedenSpelit/leds.cpp
@@ -1,36 +1,65 @@
-#include "leds.h"
+void setup() {
+  // // Määritellään LEDien pinnit
+const int ledPins[] = {A2, A3, A4, A5}; // LEDit kytketty analogiapinneihin A2-A5
+int currentLed = 0;  // Tietoa siitä, mikä LED on tällä hetkellä päällä
 
-
-void initializeLeds()
-{
-// see requirements for this function from leds.h
+// Alustetaan LEDit
+void initializeLeds() {
+    for (int i = 0; i < 4; i++) {
+        pinMode(ledPins[i], OUTPUT);
+    }
+    clearAllLeds();  // Sammutetaan kaikki LEDit alussa
 }
 
-void setLed(byte ledNumber)
-{
-// see requirements for this function from leds.h
-
+// Sytyttää tietyn LEDin ja sammuttaa muut
+void setLed(int led) {
+    clearAllLeds();  // Sammutetaan kaikki LEDit ensin
+    digitalWrite(ledPins[led], HIGH);  // Sytytetään valittu LED
+    currentLed = led;  // Päivitetään nykyinen LED
 }
 
-
-void clearAllLeds()
-{
-// see requirements for this function from leds.h
- 
+// Sammutetaan kaikki LEDit
+void clearAllLeds() {
+    for (int i = 0; i < 4; i++) {
+        digitalWrite(ledPins[i], LOW);
+    }
 }
 
-void setAllLeds()
-{
-// see requirements for this function from leds.h
+// Sytyttää kaikki LEDit
+void setAllLeds() {
+    for (int i = 0; i < 4; i++) {
+        digitalWrite(ledPins[i], HIGH);
+    }
 }
 
-
-void show1()
-{
-// see requirements for this function from leds.h
+// Näyttää yksinkertaisen valoshown ennen peliä
+void show1() {
+    for (int i = 0; i < 4; i++) {
+        setLed(i);
+        delay(500);  // Pieni viive, jotta käyttäjä voi nähdä valon
+    }
 }
 
-void show2(int rounds)
-{
-// see requirements for this function from leds.h  
+// Näyttää nopeutuvan valoshown
+void show2() {
+    for (int i = 0; i < 4; i++) {
+        setLed(i);
+        delay(250);  // Nopeampi viive
+    }
+    setAllLeds();
+    delay(100);  // Viimeinen nopea välähdys
+    clearAllLeds();
+}
+
+void setup() {
+    initializeLeds();  // Alustetaan LEDit
+    show1();  // Ajetaan valoshow1 ennen peliä
+}
+
+void loop() {
+    // Tämä looppi on pelin aikana käytössä. Esimerkiksi valitaan syttyvä LED ja odotetaan käyttäjän syötettä.
+    for (int i = 0; i < 4; i++) {
+        setLed(i);  // Sytytetään yksitellen LEDit
+        delay(1000);  // Viive, jotta LED palaa hetken
+    }
 }
